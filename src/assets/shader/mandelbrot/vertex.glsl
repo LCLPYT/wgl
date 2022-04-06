7a095984fbd5c856ba155b43d3b2d237211c2fcc
LCLP <lukas@lclpnet.work>
2022-04-06 22:01:05 +0200

    GitHub Pages Setup

--- src/assets/shader/mandelbrot/vertex.glsl
+++
@@ -1,6 +0,0 @@
-#version 100
-attribute vec3 coordinates;
-
-void main() {
-    gl_Position = vec4(coordinates, 1);
-}
--- src/views/HomeView.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import Canvas from "@/components/Mandelbrot.vue";</script>
-
-<template>
-  <main>
-    <Canvas />
-  </main>
-</template>
